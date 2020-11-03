e8c684310b14b127c8cda0f462a83a8afc19b4fe
Rao Moueed Ahmed <moueedrao@gmail.com>
2020-11-03 15:11:41 +0500

    Add YoutubeAPI Library and Config class

---
+++ app/src/main/java/com/amoueed/youtubeapitestapp/YoutubeConfig.java
@@ -0,0 +1,13 @@
+package com.amoueed.youtubeapitestapp;
+
+public class YoutubeConfig {
+
+    private static final String API_KEY = "AIzaSyBxw4VvpwpPftVEpO91rmKoSzfEe2pD3wU";
+
+    public YoutubeConfig() {
+    }
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
